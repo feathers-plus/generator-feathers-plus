f327dbbd57f11e01a13f0b5b8358d915573803dc
John Szwaronek <johnsz9999@gmail.com>
2018-04-25 14:26:05 -0400

    Fixed strigification of prop names like ^[a-zA-Z_][a-zA-Z0-9_]*:

---
+++ test/1/stringify-plus.test.js
@@ -0,0 +1,15 @@
+
+const { assert } = require('chai');
+
+const stringify = require('../../lib/stringify-plus');
+
+describe('stringify-plus', () => {
+  it('handles non-variable prop name', () => {
+    const str = stringify(
+      { '^[a-zA-Z_][a-zA-Z0-9_]*$': { type: 'number' } },
+      { stringifyIndents: 2 });
+
+    assert.equal(str,
+      `{\n  "^[a-zA-Z_][a-zA-Z0-9_]*$": {\n    type: "number"\n  }\n}`);
+  });
+});
