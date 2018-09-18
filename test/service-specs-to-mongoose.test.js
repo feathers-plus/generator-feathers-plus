102944903f2d727c8e546fb8b61e630cd84dc377
gowthaman-i2i <gowthaman@ideas2it.com>
2018-09-18 12:01:13 +0530

    Added default property  support for mongoose; refactored tests

--- test/service-specs-to-mongoose.test.js
+++ test/service-specs-to-mongoose.test.js
@@ -2,6 +2,8 @@
 const serviceSpecsToMongoose = require('../lib/service-specs-to-mongoose');
 const assert = require('assert');
 const usersSchema = require('./helpers/users.schema-enum');
+const moviesSchema = require('./helpers/movie.schema-default');
+
 
 describe('Creates Mongoose Schemas', function () {
   it('includes enum attributes', function () {
@@ -12,5 +14,15 @@ describe('Creates Mongoose Schemas', function () {
     assert.deepEqual(mongooseSchema.roles1.enum, [ 'admin' ], 'roles1 enum was correctly applied');
 
     assert.deepEqual(mongooseSchema.dept.enum, [ 'acct' ], 'dept enum was correctly applied');
+  });
+  it('includes default attributes', function () {
+    const mongooseSchema = serviceSpecsToMongoose(moviesSchema.schema);
+
+    assert.deepEqual(mongooseSchema.status.default,'active', 'staus default value was correctly applied');
+
+    assert.deepEqual(mongooseSchema.genre.default,'action', 'genre default value was correctly applied');
+    
+    assert.deepEqual(mongooseSchema.registerdDate.default, Date.now, 'registerdDate default value was correctly applied');
+   
   });
 });
