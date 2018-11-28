64e41440d002f5f83455e925b87f08d4714c0ba4
lukeburpee <lukeburpee@gmail.com>
2018-11-28 16:18:30 -0500

    fix to update tslint on semicolon option change

--- generators/writing/index.js
+++ generators/writing/index.js
@@ -383,17 +383,39 @@ module.exports = function generatorWriting (generator, what) {
     const rules = eslintrc.rules = eslintrc.rules || {};
     const rulesSemi = rules.semi;
 
+    // Modify tslint.json for semicolon option
+    let tslintExists = true;
+    let tslintJsonChanged = false;
+    let tslintjson = generator.fs.readJSON(join(process.cwd(), 'tslint.json'), {});
+
+     if (!Object.keys(tslintjson).length) {
+      tslintExists = false;
+      tslintjson = generator.fs.readJSON(join(tpl, '_tslint.json'), {});
+    }
+
+    const tsRules = tslintjson.rules = tslintjson.rules || {};
+    const tsRulesSemi = tsRules.semicolon;
+
+
     if (context.sc) {
       // semicolons used
       if (!Array.isArray(rulesSemi) || rulesSemi[0] !== 'error') {
         eslintrc.rules.semi = ['error', 'always'];
         eslintrcChanged = true;
       }
+      if (!Array.isArray(tsRulesSemi) || tsRulesSemi[0] !== true) {
+        tslintjson.rules.semicolon = true;
+        tslintJsonChanged = true;
+      }
     } else {
       // semicolons not used
       if (rulesSemi) {
         delete rules.semi;
         eslintrcChanged = true;
+      }
+      if (tsRulesSemi) {
+        tslintjson.rules.semicolon = false;
+        tslintJsonChanged = true;
       }
     }
 
@@ -459,7 +481,7 @@ module.exports = function generatorWriting (generator, what) {
       );
     } else {
       todos = todos.concat(
-        copy([tpl, 'tslint.json'], 'tslint.json', true),
+        json(tslintjson, 'tslint.json', null, tslintExists && !tslintJsonChanged),
         tmpl([tpl, 'tsconfig.json'], 'tsconfig.json', true),
         copy([tpl, 'tsconfig.test.json'], 'tsconfig.test.json', true),
       );
