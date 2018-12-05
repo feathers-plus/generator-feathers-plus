558ae3913861ae12f7c85402469ec89e52425e7a
John Szwaronek <johnsz9999@gmail.com>
2018-12-05 13:20:06 -0500

    Fixed all tests. All PRs merged.

--- test-expands/a-gens/ts/test-authentication.test-expected/src1/seed-data.ts
+++ test-expands/a-gens/ts/test-authentication.test-expected/src1/seed-data.ts
@@ -2,7 +2,6 @@
 /* tslint:disable:no-console */
 import { join } from 'path';
 import { readJsonFileSync } from '@feathers-plus/test-utils';
-import config from '../config/default.json';
 import { App } from './app.interface';
 // !code: imports // !end
 
@@ -10,8 +9,10 @@ import { App } from './app.interface';
 let ifSeedServices = ['--seed', '-s'].some(str => process.argv.slice(2).includes(str));
 
 // Determine if environment allows test to mutate existing DB data.
-let env = (config.tests || {}).environmentsAllowingSeedData || [];
-let ifDbChangesAllowed = env.includes(process.env.NODE_ENV);
+function areDbChangesAllowed(testConfig) {
+  let { environmentsAllowingSeedData = [] } = testConfig;
+  return environmentsAllowingSeedData.includes(process.env.NODE_ENV);
+}
 
 // Get generated fake data
 let fakeData = readJsonFileSync(join(__dirname, '../seeds/fake-data.json')) || {};
@@ -21,6 +22,7 @@ let services = (readJsonFileSync(join(__dirname, '../feathers-gen-specs.json')) 
 // !code: init // !end
 
 export default async function (app: App) {
+  const ifDbChangesAllowed = areDbChangesAllowed(app.get("tests"));
   // !code: func_init // !end
   if (!ifSeedServices) return;
   if (!ifDbChangesAllowed) return;
