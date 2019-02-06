7a108b41e31be3518a670b5385897e3ab8e196af
lukeburpee <lukeburpee@gmail.com>
2019-02-06 16:25:24 -0500

    Updates to allow codelist output file
    - Move all write logic to generatorWriting
    - Add basic prompt for codelist output
    - Add break for codelist what in spec.js

--- generators/codelist/index.js
+++ generators/codelist/index.js
@@ -1,24 +1,54 @@
 
 const chalk = require('chalk');
+const makeDebug = require('debug');
 const { cwd } = require('process');
 const { parse } = require('path');
 
 const Generator = require('../../lib/generator');
-const { getFragments, formatCodelist, flattenCodelist } = require('../../lib/code-fragments');
+const generatorWriting = require('../writing');
+
+const debug = makeDebug('generator-feathers-plus:prompts:codelist');
 
 module.exports = class CodelistGenerator extends Generator {
   async prompting () {
     await Generator.asyncInit(this);
+    const { _specs: specs } = this;
+    const generator = this;
+
+    const { ts } = specs.options;
+    const jsFormat = ts ? 'ts' : 'js';
+
+    const prompts = [{
+      type: 'list',
+      name: 'format',
+      message: 'What codelist output format?',
+      default: 'console',
+      choices: () => [
+        { name: 'console', value: 'console' },
+        { name: jsFormat, value: jsFormat },
+        { name: 'json', value: 'json' }
+      ]
+    }];
+    return this.prompt(prompts)
+    .then(answers => {
+      Object.assign(this.props, answers, {});
+
+        // Set missing defaults when call during test
+        if (this._opts.calledByTest && this._opts.calledByTest.prompts) {
+          this.props = Object.assign({}, this._opts.calledByTest.prompts, this. props);
+        }
+
+        debug('codelist prompting() ends', this.props);
+
+        if (!generator.callWritingFromPrompting()) return;
+
+        debug('codelist writing patch starts. call generatorWriting');
+        generatorWriting(generator, 'codelist');
+        debug('codelist writing patch ends');
+    });
   }
 
   writing () {
-    const code = getFragments();
-    const dirLen = process.cwd().length + 1;
-
-    let codelist;
-
-    codelist = formatCodelist(code, dirLen);
-
     this.log();
     this.log([
       chalk.green.bold('The custom code found in generated modules in dir '),
@@ -26,6 +56,8 @@ module.exports = class CodelistGenerator extends Generator {
       ':',
     ].join(''));
 
-    codelist = flattenCodelist(codelist, this.log);
+    if (this.callWritingFromPrompting()) return;
+
+    generatorWriting(this, 'codelist');
   }
 };
