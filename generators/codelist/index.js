f163b30d852d5fb59c7f31f339a3e47a03a6df62
Eddyystop <eddyystop@users.noreply.github.com>
2019-02-07 13:35:54 -0500

    Revert "Updates to allow custom resources in codelist"

--- generators/codelist/index.js
+++ generators/codelist/index.js
@@ -4,7 +4,7 @@ const { cwd } = require('process');
 const { parse } = require('path');
 
 const Generator = require('../../lib/generator');
-const { getFragments, formatCodelist, flattenCodelist } = require('../../lib/code-fragments');
+const { getFragments } = require('../../lib/code-fragments');
 
 module.exports = class CodelistGenerator extends Generator {
   async prompting () {
@@ -15,10 +15,6 @@ module.exports = class CodelistGenerator extends Generator {
     const code = getFragments();
     const dirLen = process.cwd().length + 1;
 
-    let codelist;
-
-    codelist = formatCodelist(code, dirLen);
-
     this.log();
     this.log([
       chalk.green.bold('The custom code found in generated modules in dir '),
@@ -26,6 +22,18 @@ module.exports = class CodelistGenerator extends Generator {
       ':',
     ].join(''));
 
-    codelist = flattenCodelist(codelist, this.log);
+    Object.keys(code).forEach(filePath => {
+      const codeFilePath = code[filePath];
+
+      this.log();
+      this.log(chalk.yellow.bold(`// !module ${filePath.substr(dirLen)}`));
+      this.log();
+
+      Object.keys(codeFilePath).forEach(codeLocation => {
+        this.log(chalk.green.bold(`// !code: ${codeLocation}`));
+        this.log(codeFilePath[codeLocation].join('\n'));
+        this.log(chalk.green.bold('// !end'));
+      });
+    });
   }
 };
